c3b6bab5b4e374bfa8b37c75eb24da56dff61d00
Jimmy Chuk <jimmychuk@outlook.com>
2021-11-12 10:46:34 -0500

    useEffect practice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedUserLogggedInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLogggedInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    //this is a browser independt function, not react
+    //takes in two arguments, two string values - any item identifier string, any string can be words too
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
