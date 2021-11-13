a7c7b7a06826e7bd8b6bffefbb3ebe1dbea276d7
Jimmy Chuk <jimmychuk@outlook.com>
2021-11-13 16:25:32 -0500

    refactoring props to props handling with REACT context hook

--- src/App.js
+++ src/App.js
@@ -32,10 +32,12 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider value={{
+    <AuthContext.Provider 
+    value={{
       isLoggedIn: isLoggedIn,
+      onLogout: logoutHandler
     }}>
-      <MainHeader onLogout={logoutHandler} />
+      <MainHeader />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
